d166647be12748a139ab1948e0ae576c429f973c
bl4ck4ndbr0wn <ju5tu5.qu35t@gmail.com>
2018-06-29 00:25:11 +0300

    [Feature #158708117] Installing service worker.

--- src/js/main.js
+++ src/js/main.js
@@ -8,6 +8,19 @@
 
 import Forms from "./components/Form";
 
+async function onLoadSWAsync() {
+  //Install the service worker
+  if ("serviceWorker" in navigator) {
+    try {
+      let serviceWorker = await navigator.serviceWorker.register("/sw.js");
+      console.log(`Service worker registered ${serviceWorker}`);
+    } catch (err) {
+      console.error(`Failed to register service worker: ${err}`);
+    }
+  }
+}
+onLoadSWAsync();
+
 //  Initializing the classes
 const forms = new Forms();
 
