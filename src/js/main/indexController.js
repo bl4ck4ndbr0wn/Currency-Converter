a15830055ca99b49ffd6aae541696171a5ee383a
bl4ck4ndbr0wn <ju5tu5.qu35t@gmail.com>
2018-06-30 15:43:52 +0300

    [Bug #158743333] Registering service worker now working(handled Promises)

--- src/js/main/indexController.js
+++ src/js/main/indexController.js
@@ -1,46 +1,58 @@
 if ("serviceWorker" in navigator) {
-  // Register the service worker
-  navigator.serviceWorker
-    .register("./../sw.js")
-    .then(reg => {
-      console.log("Service worker registered! on scope ", reg.scope);
-      //if no controller, page wasn't loaded via service worker, so its latest version.(exit early)
-      if (!navigator.serviceWorker.controller) {
-        return;
-      }
-      //if worker is waiting call update ready
-      if (reg.waiting) {
-        _updateReady();
-      }
-      //if worker is installing track progress
-      if (reg.installing) {
-        _trackInstalling();
-        return;
-      }
-      //otherwise listen for new worker arriving.
-      reg.addEventListener("updatefound", () => {
-        _trackInstalling(reg.installing);
-      });
-    })
-    .catch(err => console.log("Service worker not registered!", err));
+  navigator.serviceWorker.register("./../sw.js").then(reg => {
+    console.log(`Service Worker Registered on scope ${reg.scope}`);
+  });
 }
 
-const _trackInstalling = worker => {
-  worker.addEventListener("statechange", () => {
-    if (worker.state == "installed") {
-      _updateReady();
-    }
-  });
-};
+// import api from "./../api";
+// import idb from "idb";
 
-const _updateReady = () => {
-  let newWorker;
+// function openDatabase() {
+//   if (!navigator.serviceWorker) {
+//     return Promise.resolve();
+//   }
+//   return idb.open("CConverter", 1, upgradeDB => {
+//     const store = upgradeDB.createObjectStore("CConverter", {
+//       keyPath: "id"
+//     });
+//     store.createIndex("currency", "countries");
+//   });
+// }
 
-  newWorker = reg.installing;
-  const notification = document.getElementById("notification");
-  notification.className = "show";
-  // The click event on the pop up notification
-  document.getElementById("reload").addEventListener("click", function() {
-    navigator.serviceWorker.controller.postMessage({ action: "skipWaiting" });
-  });
-};
+// const _trackInstalling = worker => {
+//   worker.addEventListener("statechange", () => {
+//     if (worker.state == "installed") {
+//       _updateReady();
+//     }
+//   });
+// };
+
+// const _updateReady = () => {
+//   let newWorker;
+//   const notification = document.getElementById("notification");
+//   notification.className = "show";
+//   // The click event on the pop up notification
+//   document.getElementById("reload").addEventListener("click", () => {
+//     navigator.serviceWorker.controller.postMessage({ action: "skipWaiting" });
+//   });
+// };
+// // Ensure refresh is only called once.
+// // This works around a bug in "force update on reload".
+
+// //if no controller, page wasn't loaded via service worker, so its latest version.(exit early)
+// // if (!navigator.serviceWorker.controller) {
+// //   return;
+// // }
+// // //if worker is waiting call update ready
+// // if (reg.waiting) {
+// //   _updateReady();
+// // }
+// // //if worker is installing track progress
+// // if (reg.installing) {
+// //   _trackInstalling();
+// //   return;
+// // }
+// // //otherwise listen for new worker arriving.
+// // reg.addEventListener("updatefound", () => {
+// //   _trackInstalling(reg.installing);
+// // });
